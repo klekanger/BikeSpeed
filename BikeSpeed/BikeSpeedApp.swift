edba2921d036e32efc246d912950f1a9f8269db3
Kurt Lekanger <kurt@lekanger.no>
2026-07-04 17:55:37 +0200

    Initial Commit

---
+++ BikeSpeed/BikeSpeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeSpeedApp.swift
+//  BikeSpeed
+//
+//  Created by Kurt Lekanger on 04/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct BikeSpeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeSpeed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BikeSpeed
+//
+//  Created by Kurt Lekanger on 04/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
